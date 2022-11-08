a2f8cef56f0e96dbcd9072add088f8c1d4422495
Kevin <kevin12kim27@gmail.com>
2022-11-07 23:01:28 -0800

    initial ADC driver code

---
+++ mcu_code/STM32L432KC_ADC.h
@@ -0,0 +1,18 @@
+// STM32L432KC_GPIO.h
+// Header for GPIO functions
+
+#ifndef STM32L4_ADC_H
+#define STM32L4_ADC_H
+
+#include <stdint.h> // Include stdint header
+#include <stm32l432xx.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// Definitions
+///////////////////////////////////////////////////////////////////////////////
+void initADC();
+
+void calibrate();
+
+
+#endif
--- mcu_code/main.c
+++ mcu_code/main.c
@@ -12,6 +12,8 @@ Purpose : Generic application start
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "STM32L432KC.h"
+#include <stm32l432xx.h>
 
 /*********************************************************************
 *
@@ -21,6 +23,17 @@ Purpose : Generic application start
 *   Application entry point.
 */
 int main(void) {
+
+    //turn on clk to GPIOA
+    RCC->AHB2ENR |= _VAL2FLD(RCC_AHB2ENR_GPIOAEN,0b1);
+
+    //set pin A0 to analog mode (this is done on RESET)
+    GPIOA->MODER |= _VAL2FLD(GPIO_MODER_MODE0_0,0b0);
+
+    //configure channel 5 for adc regular sequence thing
+    ADC1->SQR1 |= _VAL2FLD(ADC_SQR1_SQ1,0x5);
+    initADC();
+    calibrate()
   
 }
 
