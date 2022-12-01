7787e4f7f0c49d03d9f014606bbd60e97b5967d0
Kevin Kim <kevindkim723@gmail.com>
2022-12-01 09:28:23 +0000

    SPI and FFT to SPI modules

--- fpga_code/spi.sv
+++ fpga_code/spi.sv
@@ -1,2 +1,56 @@
-module spi();
+module fft_spi(input  logic sck,//from mcu
+               input  logic sdi,//from mcu
+               output logic sdo,//to mcu
+               input  logic cs, //from mcu
+               output logic [1023:0] q, //fft input
+               input  logic [1023:0] d); //fft output
+
+    logic         qdelayed, wasdone;
+               
+    always_ff @(posedge sck)
+        if (cs) q <= {q[1022:0], sdi};
+        else    q <= {d[1022:0],sdi};
+
+    // sdo should change on the negative edge of sck
+    always_ff @(negedge sck) begin
+        qdelayed = q[1023];
+    end
+    
+    // when cs is first asserted, shift out msb before clock edge
+    assign sdo = (cs) ? qdelayed : d[1023];
+endmodule
+
+//every pulse of slow_clk, we will have a new 32 bit output from FFT
+module fft_out_flop(
+    input logic clk, //from FPGA
+    input logic [31:0] fft_out32, //from FFT
+    input logic fft_done, //from FFT
+    input logic fft_load, //from FFT
+    input logic reset,
+    output logic [1023:0] fft_out1024, 
+    output logic buf_ready
+);
+
+logic [6:0] cnt;
+logic [1023:0] q;
+logic [1023:0] d;
+logic [1023:0] d_shift;
+
+assign d_shift = (cnt == 63) ? q : q << 32;
+assign d = (cnt==0) ? 0 : {d_shift[1023:32], fft_out32}; //NOTE: we don't need the (cnt == 0) here if we pulse reset before doing everything
+
+always_ff @(negedge clk) begin
+    if (reset || fft_load || !fft_done || cnt == 64) cnt <= 0; //NOTE: done signal sketch here
+    else cnt <= cnt + 1;
+end
+
+always_ff @(negedge clk) begin
+    if (reset) q <= 0;
+    else q <= d;
+end
+
+
+assign fft_out1024 = (cnt == 0) ? 0 : q;
+assign buf_ready = (cnt == 64);
+
 endmodule
---
+++ fpga_code/testbench_spi.sv
@@ -0,0 +1,85 @@
+// Testbench taken from https://github.com/AlecVercruysse/fft_tutorial
+// and modified for a 64-point fft that uses ICE40 BRAM (1 cycle of latency per iteration)
+module fft_testbench_spi();
+   
+   logic clk, ram_clk, slow_clk;
+   logic start, load, done, reset;
+   logic signed [15:0] expected_re, expected_im, wd_re, wd_im;
+   logic [31:0]        rd, wd;
+   logic [31:0]        idx, out_idx, expected;
+
+   logic [5:0]            rd_adr;
+   assign rd_adr = idx[5:0];
+   
+   logic [31:0]          input_data [0:63];
+   logic [31:0]        expected_out [0:63];
+
+   logic fft_out_flop_done;
+   logic [1023:0] fftout1024;
+
+   integer             f; // file pointer
+
+   fft_controller dut(clk, reset, start, load, rd_adr, rd, done, wd);
+   fft_out_flop fftout(.clk(slow_clk), .reset(reset), .fft_done(done), .buf_ready(fft_out_flop_done), .fft_out1024(fftout1024), .fft_load(load), .fft_out32(wd));
+   
+   // clk
+   always
+     begin
+	    clk = 1; #5; clk=0; #5;
+     end
+
+   always
+     begin
+	    ram_clk = 1; #10; ram_clk=0; #10;
+     end
+
+   always
+     begin
+	    slow_clk = 1; #20; slow_clk=0; #20;
+     end
+   
+   // start of test: load `input_data`, `expected_out`, open output file, reset fft module.
+   initial
+     begin
+	$readmemh("simulation/test_in_square.memh", input_data);
+	$readmemh("simulation/gt_test_out_square.memh", expected_out);
+        f = $fopen("simulation/test_out_square.memh", "w"); // write computed values.
+	idx=0; reset=1; #40; reset=0;
+     end	
+
+   // increment testbench counter and derive load/start signals
+   always @(posedge slow_clk)
+     if (~reset) idx <= idx + 1;
+     else idx <= idx;
+   assign load =  idx < 64;
+   assign start = idx === 64;
+
+   // increment output address if done, reset if restarting FFT
+   always @(posedge slow_clk)
+     if (load) out_idx <= 0;
+     else if (done) out_idx <= out_idx + 1;
+   
+   // load/start logic
+   assign rd = load ? input_data[idx[5:0]] : 0;  // read in test data by addressing `input_data` with `idx`.
+   assign expected = expected_out[out_idx[5:0]]; // get test output by addressing `expected_out` with `idx`.
+   assign expected_re = expected[31:16];   // get real      part of `expected` (gt output)
+   assign expected_im = expected[15:0];         // get imaginary part of `expected` (gt output)
+   assign wd_re = wd[31:16];               // get real      part of `wd` (computed output)
+   assign wd_im = wd[15:0];                     // get imaginary part of `wd` (computed output)
+
+   // if FFT is done, compare gt to computed output, and write computed output to file.
+   always @(posedge slow_clk)
+     if (done) begin
+	if (out_idx <= (63)) begin
+           $fwrite(f, "%h\n", wd);
+	   if (wd !== expected) begin
+	      $display("Error @ out_idx %d: expected %b (got %b)    expected: %d+j%d, got %d+j%d", 
+                       out_idx, expected, wd, expected_re, expected_im, wd_re, wd_im);
+	   end
+	end else begin
+	   $display("FFT test complete.");
+           $fclose(f);
+           $stop;
+	end
+     end
+endmodule // fft_testbench
